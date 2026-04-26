3a9fafcc5f232686ab31fbfc09788ea9fc363b80
Prathmesh Prabhakar Surwase <prathmeshsurwase@Prathmeshs-MacBook-Air.local>
2026-04-26 18:58:23 +0530

    update

--- src/Landing/Pricing/Brokerage.jsx
+++ src/Landing/Pricing/Brokerage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Brokerage = () => {
   return (
-    <div>Brokerage</div>
-  )
-}
+    <div className="container mt-5">
+      <h2 style={{marginLeft : "50px"}}>Charges explained</h2>
+      <div className="row d-flex justify-content-around">
+      <div className="col-md-5 mt-4">
+        <h6>Securities/Commodities transaction tax</h6>
+        <p>
+          Tax by the government when transacting on the exchanges. Charged as
+          above on both buy and sell sides when trading equity delivery. Charged
+          only on selling side when trading intraday or on F&O.
+        </p>
+        <p>
+          When trading at Zerodha, STT/CTT can be a lot more than the brokerage
+          we charge. Important to keep a tab.
+        </p>
+        <h6>Transaction/Turnover Charges</h6>
+        <p>
+          Charged by exchanges (NSE, BSE, MCX) on the value of your
+          transactions.
+        </p>
+        <p>
+          BSE has revised transaction charges in XC, XD, XT, Z and ZP groups to
+          ₹10,000 per crore w.e.f 01.01.2016. (XC and XD groups have been merged
+          into a new group X w.e.f 01.12.2017)
+        </p>
+      </div>  
+      <div className="col-md-5 mt-4">
+        <h6>GST</h6>
+       
+        <p>
+          Tax levied by the government on the services rendered. 18% of ( brokerage + SEBI charges + transaction charges)
+        </p>
+        <h6>SEBI Charges</h6>
+        <p>
+          Charged at ₹10 per crore + GST by Securities and Exchange Board of India for regulating the markets.
+        </p>
+       
+      </div>  
+      </div>
+    </div>
+  );
+};
 
-export default Brokerage
+export default Brokerage;
--- src/Landing/Pricing/Hero.jsx
+++ src/Landing/Pricing/Hero.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='container'>
+      <h1 className='text-center m-3'>Charges</h1>
+      <p className='text-center'>List of all charges and taxes</p>
+      <div className='row mt-5'>
+        <div className='col-lg-4 col-md-6 mb-4 text-center'>
+         <img src="pricing0.svg" alt="" />
+         <h5>Free equity delivery</h5>
+          <p>
+           All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.  </p>  
+        </div>
+        <div className='col-lg-4 col-md-6 mb-4 text-center'>
+         <img src="intradayTrades.svg" alt="" />
+         <h5>Intraday and F&O trades</h5>
+          <p>
+           Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades. </p>  
+        </div>
+        <div className='col-lg-4 col-md-6 mb-4 text-center'>
+         <img src="pricing0.svg" alt="" />
+         <h5>Free direct MF</h5>
+          <p>
+          All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.    </p>  
+        </div>
+        </div>
+          <div className="col-md-8 mt-5 text-center mx-auto">
+          <h1>Open a Zerodha account</h1>
+          <h3>Modern platform and apps , $0 investment , and flat $20 intraday and F&O trades</h3>
+         
+        <button className='btn btn-secondary' style={{background : "#387ED1"}}>investment offerings →</button>
+      
+        </div>
+
+    </div>
   )
 }
 
